3b694596d4b899c5d1c1dacf491a381d54f10b64
kmmins <kmmins@yandex.ru>
2023-08-07 17:27:38 +0300

    fix: fast fix 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
         int requestLimit = 1;
         TimeUnit timeUnit = TimeUnit.SECONDS;
 
